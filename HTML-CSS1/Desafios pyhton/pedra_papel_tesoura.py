14dbfb002093b3e3914b91eaca48f766d6b9ca46
d17zzz <davi.ramiri07@gmail.com>
2026-03-18 00:54:34 -0300

    terminado

--- HTML-CSS1/Desafios pyhton/pedra_papel_tesoura.py
+++ HTML-CSS1/Desafios pyhton/pedra_papel_tesoura.py
@@ -1,6 +1,6 @@
-print("--------------------------------------------------------------------------------")
-print("Bem vindos ao jogo Pedra,Papel e tesoura, de um computador versus o humano!")
-print("--------------------------------------------------------------------------------")
+print("-------------------------------------------------------------------------------------------------")
+print("Bem vindos ao jogo Pedra,Papel e tesoura, de um computador versus o humano! (MELHOR DE 3)---------")
+print("-------------------------------------------------------------------------------------------------")
 
 import random
 import time
@@ -8,56 +8,71 @@ time.sleep(2)
 
 opcoes = ["Pedra", "Papel", "Tesoura"]
 
-escolha = input("QUAL A SUA ESCOLHA?")
-escolha_maquina = random.choice(opcoes)
-escolha_v = None ##VISUAL DELAS 🪨 Pedra, 📄 Papel, ✂️ Tesoura---
-escolha_maquina_v = None ##VISUAL DELAS
+vjogador = 0 #vitorias do jogador
+vmaquina = 0 #vitorias da maquina
 
 
-escolha.lower()
-escolha_maquina.lower()
-
-time.sleep(2)
-print("ESCOLHENDO O MEU...")
-time.sleep(2)
-
-if escolha == "pedra":
-    escolha_v = "🪨"
-elif escolha == "papel":
-    escolha_v = "📄"
-elif escolha == "tesoura":
-    escolha_v = "✂️"
+while (vjogador or vmaquina)< 3:
+    escolha = input("QUAL A SUA ESCOLHA?")
+    escolha_maquina = random.choice(opcoes)
+    escolha_v = None ##VISUAL DELAS 🪨 Pedra, 📄 Papel, ✂️ Tesoura---
+    escolha_maquina_v = None ##VISUAL DELAS
 
 
-if escolha_maquina == "pedra":
-    escolha_maquina_v = "🪨"
-elif escolha_maquina == "papel":
-    escolha_maquina_v = "📄"
-elif escolha_maquina == "tesoura":
-    escolha_maquina_v = "✂️"
+    escolha = escolha.lower()
+    escolha_maquina = escolha_maquina.lower()
 
-print(f"Jogador: {escolha_v} | Máquina: {escolha_maquina_v}")
+    time.sleep(2)
+    print("ESCOLHENDO O MEU...")
+    time.sleep(2)
 
-time.sleep(1)
+    if escolha == "pedra":
+        escolha_v = "🪨"
+    elif escolha == "papel":
+        escolha_v = "📄"
+    elif escolha == "tesoura":
+        escolha_v = "✂️"
 
-vencedor = None
 
-regras = {
-    "pedra":"tesoura",
-    "papel":"pedra",
-    "tesoura":"papel"
-}
+    if escolha_maquina == "pedra":
+        escolha_maquina_v = "🪨"
+    elif escolha_maquina == "papel":
+        escolha_maquina_v = "📄"
+    elif escolha_maquina == "tesoura":
+        escolha_maquina_v = "✂️"
 
-if escolha == escolha_maquina:
-    vencedor = "Empate"
+    print(f"Jogador: {escolha_v} | Máquina: {escolha_maquina_v}")
 
-elif regras[escolha] == escolha_maquina:
-    vencedor = "Jogador"
+    time.sleep(1)
 
-else:
-    vencedor = "Maquina"
+    vencedor = None
 
-print(f"O VENCEDOR DESSA RODADA FOI" {vencedor})
+    regras = {
+        "pedra":"tesoura",
+        "papel":"pedra",
+        "tesoura":"papel"
+    }
+
+    if escolha == escolha_maquina:
+        vencedor = "Empate"
+
+    elif regras[escolha] == escolha_maquina:
+        vencedor = "Jogador"
+
+    else:
+        vencedor = "Maquina"
+
+    print(f"O RESULTADO DESSA RODADA FOI:{vencedor}")
+
+    if vencedor == "Jogador":
+        vjogador +=1
+
+    if vencedor == "Maquina":
+        vmaquina +=1
+
+
+
+print(f"O resultado final foi de: {vjogador} vitorias do jogador, contra {vmaquina} vitorias da maquina")
 
 
 
